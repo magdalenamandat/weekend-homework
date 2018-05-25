1360d097e1bc4db3d7060aa6a2611736eca83e0e
Magdalena <m.mandat@gmail.com>
2018-05-25 15:31:37 +0100

    First commit

---
+++ pet_shop.rb
@@ -0,0 +1,46 @@
+def pet_shop_name(pet_shop)
+  p pet_shop[:name]
+end
+
+
+def total_cash(pet_shop)
+  p pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, cash)
+
+  pet_shop[:admin][:total_cash] += cash
+
+end
+
+def pets_sold(pet_shop)
+  p pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, sold)
+  pet_shop[:admin][:pets_sold] += sold
+end
+
+def stock_count(pet_shop)
+  p pet_shop[:pets].length
+end
+
+def pets_by_breed(pet_shop, breed)
+  total_breed = []
+  for pet in pet_shop[:pets]
+    if breed == pet[:breed]
+      total_breed.push(pet)
+    end
+  end
+  return total_breed
+  p total_breed
+end
+
+def find_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if name == pet[:name]
+      return pet
+    end
+  end
+  return nil 
+end
